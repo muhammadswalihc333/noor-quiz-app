114b78e40d62f5776d32489e1dfbd969cf11deee
muhammadswalihc333 <muhammadswalihc333@gmail.com>
2026-08-17 07:28:13 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
-const CACHE_NAME = "noor-quiz-v9-17-3-final";
-self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
-self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+const CACHE_NAME = "noor-quiz-v9-17-3";
+self.addEventListener("install", event => {
+  event.waitUntil(self.skipWaiting());
+});
+self.addEventListener("activate", event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
+});
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-  event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
-    return response;
-  }).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request, {cache:"no-store"}).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+      return response;
+    }).catch(() => caches.match(event.request))
+  );
 });
