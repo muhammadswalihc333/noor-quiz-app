be062296ac975986ec348342b27e0b33d4d0f064
muhammadswalihc333 <muhammadswalihc333@gmail.com>
2026-08-17 06:14:18 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-const CACHE='noor-quiz-v3-master-admin';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME = "noor-quiz-v9-17-3-final";
+self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
+self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
